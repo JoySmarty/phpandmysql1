a9fe70a90468de4b100ff41cd2a81f27ed77fad8
JoySmarty <smartyjoy47@gmail.com>
2024-06-27 20:35:05 +0100

    my new commit

---
+++ chapter_01/01_05/globe_bank/private/shared/staff_header.php
@@ -0,0 +1,21 @@
+<?php
+    if(!isset($page_title )){ $page_title = 'Staff Area';}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GBI - <?php echo($page_title); ?> </title>
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
+</head>
+<body>
+    <header> 
+        <h1>GBI Staff Area</h1>
+    </header>
+
+    <navigation> 
+        <ul>
+            <li>   <a href="index.php">Menu</a>   </li>
+        </ul>
+    </navigation>
